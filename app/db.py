5981c4fcc32fdf3a460738a1bf1146be075dd02a
smarak <smarak.s@divi-t.com>
2024-03-30 18:45:28 +0530

    IP change

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import threading
 
-conn_string = "postgresql+asyncpg://metro:Metro2024@13.233.193.48:5432/metro"
+conn_string = "postgresql+asyncpg://metro:Metro2024@13.233.101.243:5432/metro"
 # conn_string = "postgresql+asyncpg://vms_user:1234@localhost:5432/metro"
 
 Base = declarative_base()
